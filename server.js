ae6787d5c441314006ae3b0e6b99974825714d9b
Daniel Alejandro <alejandro.techservices@gmail.com>
2024-01-26 12:35:00 -0500

    Configure production environment and serve React build

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.use(cors());
 app.use(express.json()); // Middleware for parsing JSON bodies
 app.use('/api/projects', projectRoutes);
 
+// Serve static files from the React frontend app in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // Serve the index.html file
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 // Connect to MongoDB
 // Access restricted to current IP 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
