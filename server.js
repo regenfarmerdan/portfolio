9f211fc9bf61231cd1ebaf1076e4a684a0cf1d56
Daniel Alejandro <alejandro.techservices@gmail.com>
2024-01-26 16:51:11 -0500

    removed build from gitignore

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const mongoose = require('mongoose');
 const projectRoutes = require('./src/routes/projectRoute');
 const cors = require('cors');
 const path = require('path');
-
-
- 
  
 // Enable CORS for all routes
 app.use(cors());
@@ -26,13 +23,11 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-
 // Connect to MongoDB
 // Access restricted to current IP 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
-
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
 // });
